25d0589d6e420144f5cb82a8f74d5f0805492ca1
Pierre-Hugues Husson <phh@phh.me>
2020-11-18 19:33:15 +0530

    [SQUASHED] mt6765-common: Add Modified TrebleApp
    [TEMP] mt6765-common: Add TrebleApp
    
    * Add priv-app permissions.
    * Add interfaces.xml
    * This method is just an experiment, will find a better way in future.
    
    Signed-off-by: Ayush <ayush@disroot.org>
    Signed-off-by: wulan17 <galihgustip@gmail.com>
    
    mt6765-common: modified treble app
    
    * Changed name in settings (credits to phh still there)
    * Keep only 'IMS features' and remove other options.
    * uses new ims apk (https://github.com/eldraco19/releases/releases/tag/beta)
      with outgoing sms fix.
    * change default state of checkboxes in 'IMS features' to true.
    
    Signed-off-by: Ayush <ayush@disroot.org>
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- mt6765.mk
+++ mt6765.mk
@@ -80,8 +80,13 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.telephony.ims.xml \
     $(LOCAL_PATH)/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml \
     $(LOCAL_PATH)/permissions/com.mediatek.ims.plugin.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.mediatek.ims.plugin.xml \
-    $(LOCAL_PATH)/permissions/com.mediatek.op.ims.common.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.mediatek.op.ims.common.xml
-    
+    $(LOCAL_PATH)/permissions/com.mediatek.op.ims.common.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.mediatek.op.ims.common.xml \
+    $(LOCAL_PATH)/permissions/interfaces.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/interfaces.xml
+
+# Privapp-permissions whitelist for PhhTrebleApp
+PRODUCT_COPY_FILES += \
+       $(LOCAL_PATH)/permissions/privapp-permissions-me.phh.treble.app.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-me.phh.treble.app.xml
+
 # RCS
 PRODUCT_PACKAGES += \
     RcsService
@@ -102,7 +107,8 @@ PRODUCT_PACKAGES += \
 
 # ImsInit hack
 PRODUCT_PACKAGES += \
-    ImsInit
+    ImsInit \
+    TrebleApp
 
 # Trust HAL
 PRODUCT_PACKAGES += \
