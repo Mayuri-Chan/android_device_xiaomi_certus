e509203cc32a3353aabc273ebcc314edc644de8e
Zinadin Zidan <zidan44@pixelexperience.org>
2020-12-12 16:25:02 +0600

    certus: Build Some IMS From MTK OSS
     * Compiling from sources for compatibility with A11's libstagefright changes
    
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- device.mk
+++ device.mk
@@ -18,6 +18,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
+# IMS
+$(call inherit-product, vendor/mediatek/ims/mtk-ims.mk)
+
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
