4b3a810813b817d326060fe993112ef307bce4e8
Brad Ebinger <breadley@google.com>
2021-01-02 16:41:26 +0700

    certus: configure the RcsService to be the default RCS service.
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- device.mk
+++ device.mk
@@ -82,6 +82,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.fingerprint.xml
 
+# RCS
+PRODUCT_PACKAGES += \
+    RcsService
+
 # RROs
 PRODUCT_PACKAGES += \
     BatteryHealthOverlay
