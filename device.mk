a9ede71c961f2ae1dea25013ae6be98bf0ee9fa3
wulan17 <galihgustip@gmail.com>
2021-04-12 08:08:53 +0000

    certus: cleanup
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- device.mk
+++ device.mk
@@ -29,9 +29,6 @@ TARGET_SCREEN_WIDTH := 720
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# VNDK
-PRODUCT_TARGET_VNDK_VERSION := 28
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.a2dp.default
@@ -58,6 +55,10 @@ endif
 PRODUCT_PACKAGES += \
     power.mt6765
 
+# Prebuilt
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/dtb:dtb.img
+
 # Init
 PRODUCT_PACKAGES += \
     fstab.enableswap \
@@ -77,16 +78,13 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.touchscreen.multitouch.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.usb.host.xml \
-    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/handheld_core_hardware.xml
+    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.fingerprint.xml
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
-# Symbols 
-PRODUCT_PACKAGES += \
-    libshim_showlogo
-    
 # System properties
 -include $(LOCAL_PATH)/product_prop.mk
 
