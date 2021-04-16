fbf7a66edfb655e5278ed2c2c6ba986e20e15aad
wulan17 <galihgustip@gmail.com>
2021-04-16 22:35:07 +0000

    certus: Nusantara-Ify
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_certus.mk
+    $(LOCAL_DIR)/nad_certus.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_certus-user \
-    lineage_certus-userdebug \
-    lineage_certus-eng
+    nad_certus-user \
+    nad_certus-userdebug \
+    nad_certus-eng
