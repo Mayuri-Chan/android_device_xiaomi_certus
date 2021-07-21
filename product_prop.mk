b07fde849b24e35775aa678b666fbdcbbdd599ad
wulan17 <galihgustip@gmail.com>
2021-07-21 03:40:04 +0000

    certus: Update Maintainer string
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -41,7 +41,3 @@ PRODUCT_PRODUCT_PROPERTIES  += \
    ro.surface_flinger.vsync_event_phase_offset_ns=8300000 \
    ro.surface_flinger.vsync_sf_event_phase_offset_ns=830000 \
    ro.surface_flinger.present_time_offset_from_vsync_ns=0
-
-# maintainer
-PRODUCT_PROPERTY_OVERRIDES += \
-    org.komodo.maintainer=wulan17
