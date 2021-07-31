9e361491ac0dd1618ec96c1105d328b2bdb9b443
wulan17 <galihgustip@gmail.com>
2021-07-31 16:58:47 +0000

    certus: build BatteryHealth and ScreenRecord overlay
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- device.mk
+++ device.mk
@@ -54,6 +54,10 @@ ifneq ($(findstring lineage, $(TARGET_PRODUCT)),)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 endif
 
+PRODUCT_PACKAGES += \
+    BatteryHealthOverlay \
+    ScreenRecordOverlay
+
 # Power
 PRODUCT_PACKAGES += \
     power.mt6765
