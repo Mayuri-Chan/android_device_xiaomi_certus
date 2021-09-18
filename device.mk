648ccafe5413aa13384703bd9871da3d6ecb0a3a
wulan17 <galihgustip@gmail.com>
2021-09-18 18:18:42 +0000

    certus: drop lineage trust hal
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- device.mk
+++ device.mk
@@ -118,9 +118,5 @@ PRODUCT_PACKAGES += \
     ImsInit \
     TrebleApp
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    lineage.trust@1.0-service
-
 # Call proprietary blob setup
 $(call inherit-product-if-exists, vendor/xiaomi/mt6765-common/mt6765-common-vendor.mk)
