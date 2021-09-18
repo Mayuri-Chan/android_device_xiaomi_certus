1da85560469683622936d4d78f545a20ca4c9133
wulan17 <galihgustip@gmail.com>
2021-09-18 19:12:21 +0000

    certus: move BatteryHealth and FpsInfo config to rro_overlays
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- device.mk
+++ device.mk
@@ -113,7 +113,9 @@ PRODUCT_PACKAGES += \
 # RROs
 PRODUCT_PACKAGES += \
     RoundedCornerFW \
-    RoundedCornerSysUI
+    RoundedCornerSysUI \
+    BatteryHealthOverlay \
+    FpsInfoOverlay
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
